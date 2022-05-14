7d93d8d56fc5f13fb8c94b491b0bcc250d843353
CPlusPatch <gascraft971@gmail.com>
2022-05-14 18:01:19 +0200

    added simple test

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const {TwitterApi} = require('twitter-api-v2');
 const config = require('./config.json');
+const events = require('events');
 
+console.log("Connecting to Twitter's API...");
 const client = new TwitterApi({
     appKey: config.appKey,
 	appSecret: config.appSecret,
 	accessToken: config.accessToken,
 	accessSecret: config.accessSecret
 });
+console.log("Connected!");
 
-client.v1.tweet('This tweet was written by a bot').then((val) => {
-    console.log(val);
+/* client.v1.tweet("I'm tweetin'").then((val) => {
     console.log("success");
 }).catch((err) => {
     console.log(err);
-});
+}); */
