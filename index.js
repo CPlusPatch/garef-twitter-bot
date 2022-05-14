5c2bfa5b450b13e42768f8c07f5d9c3b3c55569a
CPlusPatch <gascraft971@gmail.com>
2022-05-14 16:04:20 +0200

    Added config file

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const {TwitterApi} = require('twitter-api-v2');
+const config = require('./config.json');
 
 const client = new TwitterApi({
-    appKey: 'cEqR9VH92UgEHM001trUPdKDo',
-    appSecret: 'uxJ7ccBYFHjjjDQ7YiHVBFu7wdO8FlZgdzm874fmuT1Cd2YWgE',
-    accessToken: '1413032007756664832-6komplzIwE81ofxmqei0b9PaIjJwP7',
-    accessSecret: 'bLtVn7Kr8HECH85KGdNKdC34fP4LUma0i9iIun3WTZ0Wg',
+    appKey: config.appKey,
+	appSecret: config.appSecret,
+	accessToken: config.accessToken,
+	accessSecret: config.accessSecret
 });
 
 client.v1.tweet('This tweet was written by a bot').then((val) => {
-    console.log(val)
-    console.log("success")
+    console.log(val);
+    console.log("success");
 }).catch((err) => {
-    console.log(err)
-})
+    console.log(err);
+});
